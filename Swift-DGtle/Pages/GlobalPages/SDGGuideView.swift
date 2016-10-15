663c84041387cbb63eba5886dfe507f72e951d80
Luminia <btrlzlm@163.com>
2016-10-15 19:43:20 +0800

    启动页

---
+++ Swift-DGtle/Pages/GlobalPages/SDGGuideView.swift
@@ -0,0 +1,21 @@
+//
+//  SDGGuideView.swift
+//  Swift-DGtle
+//
+//  Created by apple on 2016/10/14.
+//  Copyright © 2016年 luminia. All rights reserved.
+//
+
+import UIKit
+
+class SDGGuideView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Swift-DGtle/Pages/Mine/widget/SDGShareIconCell.swift
@@ -0,0 +1,24 @@
+//
+//  SDGShareIconCell.swift
+//  Swift-DGtle
+//
+//  Created by apple on 2016/10/12.
+//  Copyright © 2016年 luminia. All rights reserved.
+//
+
+import UIKit
+
+class SDGShareIconCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
